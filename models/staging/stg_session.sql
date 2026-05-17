8cf57f60e3c6a97a4dbf271ae9d7701ff31e7758
Victorino Fernández <fernandezherediavictorino@gmail.com>
2026-05-17 22:00:00 +0000

    refactor

--- models/staging/stg_session.sql
+++ models/staging/stg_session.sql
@@ -10,12 +10,12 @@
 with source as (
     select * from {{ source('raw', 'sessions') }}
     {% if is_incremental() %}
-    where _ingested_at > (select max(dest._ingested_at) from {{ this }} dest)
+        where _ingested_at > (select max(dest._ingested_at) from {{ this }} dest)
     {% endif %}
 ),
 renamed as (
     select
-        id                                                                          as session_id,
+        id                                                                              as session_id,
         event_id,
         category_id,
         circuit_id,
@@ -23,12 +23,12 @@ renamed as (
         type,
         status,
         is_sprint,
-        date::timestamp_ntz                                                         as session_date,
-        NULLIF(TRIM(track_condition), '')                                            as track_condition,
-        NULLIF(REGEXP_REPLACE(TRIM(air_condition), '[^0-9.]', ''), '')::float       as air_condition,
-        NULLIF(REGEXP_REPLACE(TRIM(humidity_condition), '[^0-9.]', ''), '')::float  as humidity_condition,
-        NULLIF(REGEXP_REPLACE(TRIM(ground_condition), '[^0-9.]', ''), '')::float    as ground_condition,
-        NULLIF(TRIM(weather_condition), '')                                          as weather_condition,
+        date::timestamp_ntz                                                             as session_date,
+        NULLIF(TRIM(track_condition), '')                                               as track_condition,
+        NULLIF(REGEXP_REPLACE(TRIM(air_condition), '[^0-9.]', ''), '')::float           as air_condition,
+        NULLIF(REGEXP_REPLACE(TRIM(humidity_condition), '[^0-9.]', ''), '')::float      as humidity_condition,
+        NULLIF(REGEXP_REPLACE(TRIM(ground_condition), '[^0-9.]', ''), '')::float        as ground_condition,
+        NULLIF(TRIM(weather_condition), '')                                             as weather_condition,
         _ingested_at
     from source
     where id is not null
